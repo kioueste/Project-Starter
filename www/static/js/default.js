be740bb7c5230a877f5b1e1a083dd64dd662fc56
Kaelig - LunaWeb <kaelig@lunaweb.fr>
2010-08-30 14:31:09 +0200

    Switched from tabbed indenting to space indenting for better cross-editor support.

--- www/static/js/default.js
+++ www/static/js/default.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-	
-	// Open external links in a new window or tab
-	$('a[rel$=external]').live('click', function() {
-		$(this).attr('target', "_blank");
-	});
-	
-	// Fix PNG transparency in IE6
-	if ($.browser.msie && $.browser.version == 6) {
-		DD_belatedPNG.fix('.png');
-	}
+    
+    // Open external links in a new window or tab
+    $('a[rel$=external]').live('click', function() {
+        $(this).attr('target', "_blank");
+    });
+    
+    // Fix PNG transparency in IE6
+    if ($.browser.msie && $.browser.version == 6) {
+        DD_belatedPNG.fix('.png');
+    }
 
 });
